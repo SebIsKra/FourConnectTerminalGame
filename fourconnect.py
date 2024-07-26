4bf312b6d5f6076726c02eb2e0937a5539c937ed
SebIsKra <sebastian.isaak.krasniqi@gmail.com>
2024-07-26 15:11:40 +0200

    Start of a Four Connects game in Terminal. Decided to put the logic in classes like 'Player' and 'Board' and defined their attributes first

--- fourconnect.py
+++ fourconnect.py
@@ -1,1 +1,30 @@
 
+#The player class and their attributes and methods
+class Player:
+    #Initialising the attributes of a the 'Player'-class
+    def __init__(self, name, color, winner=False):
+        self.name = name 
+        self.color = color
+        self.winner = winner
+
+    #Creating the logic for a player turn
+    def turn():
+        pass
+
+
+
+class Board:
+    
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.board = [['0' for _ in range(columns)] for _ in range(rows)]
+    
+    def print_board(self):
+        for row in self.board:
+            print(' '.join(row))
+
+
+board = Board(6, 7)
+
+board.print_board()
