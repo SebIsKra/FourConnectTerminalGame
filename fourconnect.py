22f9493a7cc8cd6dc00c3d13ea95db4fc1e89669
SebIsKra <sebastian.isaak.krasniqi@gmail.com>
2024-08-02 12:25:28 +0200

    Updated the game and made comments to understand methods
    Finished and working simple 2D Four Connect Game for the Terminal

--- fourconnect.py
+++ fourconnect.py
@@ -2,33 +2,35 @@ import numpy as np
 
 #The player class and their attributes and methods
 class Player:
-    #Initialising the attributes of a the 'Player'-class
     def __init__(self, name, player_tag, winner=False):
         self.name = name 
         self.player_tag = player_tag
         self.winner = winner
 
-    #Creating the logic for a player turn
+    #Make the player choose a column for the turn 
     def choose_column(self):
         while True:
             try:
                 chosen_column = int(input(f'{self.name}, where do you want to put your token? (Choose column from 1 to 7): '))
+                #Uses the input minus 1 to match the index numbers 0-6
                 if chosen_column in range(1, 8):
                     return chosen_column - 1
                 else:
-                    print("Invalid input. Please choose a valid column between 1 and 7")
+                    print("Invalid input. Please choose a valid column between 1 and 7!")
+
             except ValueError:
                 print("Invalid input. Please enter a column between 1 and 7")
 
-
+    #Creating the logic for a player turn using the methods of both classes
     def player_turn(self, board):
         column = self.choose_column()
+        #get_available_row method from the board class
         row = board.get_available_row(column)
-        if row is not None:
+        if row != None:
             board.fill_slot(row, column, self.player_tag)
             return True
         else:
-            print("Column is full! Choose another column.")
+            print("Column is full! Please choose another column.")
             return False
 
          
@@ -36,7 +38,7 @@ class Player:
 
 
 
-#The board class including the attributes of the board and it's methods for game logic
+#The board class and their attributes and their methods for game logic
 class Board:
     def __init__(self, rows=6, columns=7):
         self.rows = rows
@@ -46,9 +48,10 @@ class Board:
     
     
     def print_board(self):
-        # Flip the board for correct display, as top row should be displayed first
+        # Flip the board for correct display, as top row should be displayed first & making the index [0][0] bottom left instead of top right
         print(np.flip(self.board, 0))
 
+    #Switch out the 0 in the index with the player_tag
     def fill_slot(self, row, column, player_tag):
         self.board[row][column] = player_tag
 
@@ -58,7 +61,7 @@ class Board:
                 return row
         return None
 
-    
+    #Check the winner in the relevant locations (horizontal, vertical, diagonal)
     def check_winner(self, player_tag):
         # Check horizontal locations
         for row in range(self.rows):
@@ -72,13 +75,13 @@ class Board:
                 if np.all(self.board[row:row + 4, col] == player_tag):
                     return True
 
-        # Check positively sloped diagonals
+        # Check positively diagonal locations
         for row in range(self.rows - 3):
             for col in range(self.columns - 3):
                 if all(self.board[row + i][col + i] == player_tag for i in range(4)):
                     return True
 
-        # Check negatively sloped diagonals
+        # Check negatively diagonal locations
         for row in range(3, self.rows):
             for col in range(self.columns - 3):
                 if all(self.board[row - i][col + i] == player_tag for i in range(4)):
@@ -95,6 +98,8 @@ def __main__():
     player2 = Player("Player 2", player_tag=2)
 
     players = [player1, player2]
+
+    #Sets a turn variable to keep track of which players turn it is on even numbers the remainder is giving the turn to player1
     turn = 0
 
     while True:
